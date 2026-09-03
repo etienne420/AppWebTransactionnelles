ce292f864cfad25bb960d0d0cc7e5fc1d02ee7b0
Étienne Desaulniers <2496293@cmontmorency.qc.ca>
2026-09-03 14:03:28 -0400

    Ajout de pages autres bidons

---
+++ credits.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($nomProjet) ?></title>
+</head>
+
+<body>
+    <h1>Page crédits</h1>
+
+    <ul>
+        <li>Moi même</li>
+    </ul>
+
+
+    <nav>
+        <a href="index.php">Retour à l'accueil</a>
+    </nav>
+</body>
+</html>
