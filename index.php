78f7a3f5215de2da4e2e07751a5db63c83a89665
Étienne Desaulniers <2496293@cmontmorency.qc.ca>
2026-09-03 13:41:11 -0400

    Ajout de la page Recits

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+$nomProjet = 'Biblio de jeux'; 
+$auteur = 'Etienne Desaulniers'; 
+$versionPhp = PHP_VERSION; 
+$descriptionProjet = 'Une bibliotheque de jeux où des utilisateurs peuvent gérer leur collection de jeux vidéo, donner des évaluations et emprunter des jeux.';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($nomProjet) ?></title>
+</head>
+
+<body>
+    <h1><?= htmlspecialchars($nomProjet) ?></h1>
+
+    <p>Auteur : <?= htmlspecialchars($auteur) ?></p>
+
+    <p>Version PHP : <?= htmlspecialchars($versionPhp) ?></p>
+
+    <p>Description du projet : <?= htmlspecialchars($descriptionProjet) ?></p>
+
+    <nav>
+        <a href="recits.php">recits</a>
+    </nav>
+</body>
+</html>
