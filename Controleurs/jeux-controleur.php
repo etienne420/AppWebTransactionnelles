87acc95445fd0385fc9b7b6951084ef50974252d
Étienne Desaulniers <2496293@cmontmorency.qc.ca>
2026-09-16 11:08:28 -0400

    implémentation ajout de jeux dans la BD depuis la page jeux

--- Controleurs/jeux-controleur.php
+++ Controleurs/jeux-controleur.php
@@ -3,6 +3,18 @@
 require_once __DIR__ . '/../config/bd.php';
 require_once __DIR__ . '/../Modeles/jeu-modele.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $titre = trim($_POST['titre'] ?? '');
+    $annee_sortie = (int) ($_POST['annee_sortie'] ?? 0);
+    $genre = trim($_POST['genre'] ?? '');
+
+    addJeux($pdo, $titre, $annee_sortie, $genre);
+
+    header('Location: index.php?action=jeux');
+    exit;
+}
+
 $jeux = getJeux($pdo);
 
 require_once __DIR__ . '/../Vues/jeux/index.php';
